b2935f16d6014e45a10abb6f56b44a1adc2aa48f
Bovo01 <pietro.polenta@gmail.com>
2023-07-18 16:51:15 +0200

    Aggiungere i file di progetto.

---
+++ Graph search/Components/GraphNodeComponent.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Graph_search.Components
+{
+    public partial class GraphNodeComponent : UserControl
+    {
+        public GraphNodeComponent(string name)
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            this.NodeName = name;
+        }
+
+        public string NodeName { get; private set; }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DataObject data = new DataObject();
+                data.SetData(DataFormats.Serializable, this);
+                Point mouseOffset = Mouse.GetPosition(this);
+                data.SetData("mouseOffset", mouseOffset);
+
+                // Initiate the drag-and-drop operation.
+                DragDrop.DoDragDrop(this, data, DragDropEffects.Move);
+            }
+            e.Handled = true;
+        }
+
+        protected override void OnGiveFeedback(GiveFeedbackEventArgs e)
+        {
+            base.OnGiveFeedback(e);
+            Mouse.SetCursor(Cursors.Hand);
+            e.Handled = true;
+        }
+    }
+}
---
+++ Graph search/Model/GraphNode.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph_search
+{
+    /// <summary>
+    /// Weighted graph
+    /// </summary>
+    internal class Graph
+    {
+        internal readonly List<GraphNode> nodes;
+
+        internal Graph()
+        {
+            nodes = new List<GraphNode>();
+        }
+
+        internal GraphNode AddNode(string name)
+        {
+            GraphNode node = new GraphNode(name);
+            nodes.Add(node);
+            return node;
+        }
+        internal GraphEdge SetEdge(GraphNode node1, GraphNode node2, int weight)
+        {
+            GraphEdge edge = new GraphEdge(node1, node2, weight);
+            node1.AddLink(edge);
+            node2.AddLink(edge);
+            return edge;
+        }
+    }
+
+    /// <summary>
+    /// Graph node: contains name and edges
+    /// </summary>
+    internal class GraphNode
+    {
+        internal readonly string name;
+        internal readonly List<GraphEdge> links;
+
+        internal GraphNode(string name)
+        {
+            this.name = name;
+            links = new List<GraphEdge>();
+        }
+
+        internal void AddLink(GraphEdge link)
+        {
+            links.Add(link);
+        }
+    }
+
+    /// <summary>
+    /// Graph edge: links 2 nodes with a weight
+    /// </summary>
+    internal class GraphEdge
+    {
+        private readonly GraphNode node1, node2;
+        internal readonly int weight;
+
+        internal GraphEdge(GraphNode node1, GraphNode node2, int weight)
+        {
+            this.node1 = node1;
+            this.node2 = node2;
+            this.weight = weight;
+        }
+
+        internal GraphNode Opposite(GraphNode node)
+        {
+            if (node == node1)
+                return node2;
+            if (node == node2)
+                return node1;
+            throw new ArgumentException("invalid node");
+        }
+    }
+}
